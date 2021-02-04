e693c534c75beac6b94a9ec13a688921059e5043
Akyl20 <77338802+Akyl20@users.noreply.github.com>
2021-02-04 15:21:28 +0300

    test

--- app.py
+++ app.py
@@ -1,28 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from openpyxl import load_workbook
+
 app = Flask(__name__)
+
 @app.route("/")
 def homepage():
-    return "Тест!"
+    return render_template("index.html")
+
+
 @app.route("/books/")
 def books():
     excel = load_workbook("tales.xlsx")
     page = excel["Лист1"]
-    
-    tales = [tale.value for tale in page ["A"]][1:]
+    object_list = [[tale.value, tale.offset(column = 1).value] for tale in page["A"][1:]]
+    return render_template("books.html", object_list = object_list)
 
-    authors = [author.value for author in page ["B"]][1:]
-   
-    html = "<h1>Туд будет список книг :</h1>"
-        
+    # tales = [tale.value for tale in page["A"]][1:]
+    # tales = []
+    # for tale in page["A"][1:]:
+    #     tales.append(tale.value)
 
-    for i in range(len(tales)):
-        html += f"<h2>{tales[i]} - {authors[i]}</h2>"
-    return html    
+    # authors = [author.value for author in page["B"]][1:]   
+
+    # html = """
+    #     <a href="/authors">Авторы</a>
+    #     <a href="/books">Книги</a>
+    #     <h1 style="color: red">Тут будет список книг:</h1>
+    # """
+
+    # for i in range(len(tales)):
+    #     html += f"<h2>{tales[i]} - {authors[i]}</h2>"
+
+    # return html
 
 @app.route("/authors/")
 def authors():
     excel = load_workbook("tales.xlsx")
     page = excel["Лист1"]
     authors = {author.value for author in page["B"][1:]}
-    return render_template("authors.html")
+    return render_template(
+        "authors.html", authors=list(authors)
+    )
+
+
+@app.route("/add/", methods=["POST"])
+def add():
+    f = request.form
+    # print(f["author"], f["book"])
+    excel = load_workbook("tales.xlsx")
+    page = excel["Лист1"]
+    last = len(page["A"]) + 1
+    page[f"A{last}"] = f["book"]
+    page[f"B{last}"] = f["author"]
+    excel.save("tales.xlsx")
+
+    return "form received"
+
+@app.route("/book/<num>/")
+def book(num):
+    excel = load_workbook("tales.xlsx")
+    page = excel["Лист1"]
+    object_list = [[tale.value, tale.offset(column = 1).value, tale.offset(column = 2).value] for tale in page["A"][1:]]
+    obj = object_list[int(num)]
+    return render_template("book.html", obj = obj)
